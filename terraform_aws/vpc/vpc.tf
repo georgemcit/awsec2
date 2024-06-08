4af86df1a7e22036035a947282671e05f086491c
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 19:59:56 -0400

    Update vpc.tf

--- terraform_aws/vpc/vpc.tf
+++ terraform_aws/vpc/vpc.tf
@@ -3,8 +3,6 @@ resource "aws_vpc" "georgevpc" {
   cidr_block = var.cidr_block_vpc
   tags = var.tags
   }
-}
-
 resource "aws_subnet" "georgesb" {
   vpc_id              = var.vpc_id
   vpc_public_subnets  = var.vpc_public_subnets
