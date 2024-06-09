41963a158c9dfdc88fcf8616aae1ffdc8a43c304
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 20:58:41 -0400

    Update variable.tf

--- terraform_aws/vpc/variable.tf
+++ terraform_aws/vpc/variable.tf
@@ -1,23 +1,18 @@
-  /*         
-variable "vpc_name"{
-  type=string
-}
-variable "vpc_id"{
-  type=string
+variable "subnet_id"{
+  type  =string
 }
 variable "vpc_cidr_block"{
-  type=string
+  type  =string
 }
-variable "vpc_availability_zones"{
-  type=list(string)
+variable "sb_cidr_block"{
+  type  =string
 }
-variable "vpc_public_subnets"{
-  type=list(string)
+variable "vpc_id"{
+  type  =string
+} 
+variable "tags"{
+  type  =string
 }
-variable "vpc_private_subnets"{
-  type=list(string)
+variable "availability_zone"{
+  type  =string
 }
-variable "vpc_tags"{
-  type=string
-}
-*/
