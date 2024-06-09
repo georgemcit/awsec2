15e91d87e9ef00ad29159931075d5f6a280a0a3d
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 21:48:57 -0400

    Update module_vpc.tf

--- terraform_aws/module_vpc.tf
+++ terraform_aws/module_vpc.tf
@@ -1,6 +1,5 @@
 module "vpc" {
   source            = "./vpc/"
-  subnet_id         = "aws_subnet.george.id"
   vpc_cidr_block    = "172.16.0.0/16"
   vpc_id            = "aws_vpc.george.id"
   sb_cidr_block     = "172.16.10.0/24"
