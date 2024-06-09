d78c67289add3e8dfa5e6eec028048fb1f9c53c6
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 22:12:55 -0400

    Update main.tf

--- terraform_aws/sb/main.tf
+++ terraform_aws/sb/main.tf
@@ -1,15 +1,12 @@
 resource "aws_security_group" "vpc-ssh" {
     name="vpc-ssh"
-    description = "Dev VPC SSH"
     ingress{
-        description="Allow port 22"
         from_port = 22
         to_port = 22
         protocol = "tcp"
         cidr_blocks=["0.0.0.0/0"]
     }
     egress{
-        description = "Allow all ip and ports outbound"
         from_port=0
         to_port=0
         protocol="-1"
