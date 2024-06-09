7b2b9110628ec053cf6a72b66c65369ec29e80b4
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 21:22:58 -0400

    Update main.tf

--- terraform_aws/vpc/main.tf
+++ terraform_aws/vpc/main.tf
@@ -1,3 +1,13 @@
+resource "aws_instance" "george" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
+  cpu_options {
+    core_count       = 2
+    threads_per_core = 2
+  }
+
 resource "aws_vpc" "george" {
   cidr_block = var.vpc_cidr_block
 
