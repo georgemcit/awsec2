be18d9798749c6ada4a8c1206fbc26ca57332803
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 20:57:57 -0400

    Update main.tf

--- terraform_aws/ec2/main.tf
+++ terraform_aws/ec2/main.tf
@@ -22,20 +22,4 @@ resource "aws_instance" "george" {
     Name = var.tags
   }
 }
-resource "aws_vpc" "george" {
-  cidr_block = var.vpc_cidr_block
 
-  tags = {
-    Name = var.tags
-  }
-}
-
-resource "aws_subnet" "george" {
-  vpc_id            = var.vpc_id
-  cidr_block        = var.sb_cidr_block
-  availability_zone = var.availability_zone
-
-  tags = {
-    Name = var.tags
-  }
-}
