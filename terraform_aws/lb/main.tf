3a8b6f4c8f2dcc952e496fb875af90fd47d3f31b
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-09 09:27:44 -0400

    Update main.tf

--- terraform_aws/lb/main.tf
+++ terraform_aws/lb/main.tf
@@ -1,1 +1,19 @@
+resource "aws_lb" "george" {
+  name               = "george-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
 
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.id
+    prefix  = "george-lb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
