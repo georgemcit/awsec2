7cb1ac6dbcb805d87df8e2cf1e233035b890c09a
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 20:52:44 -0400

    Update module_ec2.tf

--- terraform_aws/module_ec2.tf
+++ terraform_aws/module_ec2.tf
@@ -1,4 +1,4 @@
 module "module_ec2t" {
-  source            = "./aws/"
+  source            = "./ec2/"
   ami               = "data.aws_ami.amzn-linux-2023-ami.id"
   instance_type     = "c6a.2xlarge"
