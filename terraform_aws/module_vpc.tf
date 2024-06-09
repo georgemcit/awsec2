8f4c8567d364fd45df1c3600dc6298f2153dfedf
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 20:49:43 -0400

    Update module_vpc.tf

--- terraform_aws/module_vpc.tf
+++ terraform_aws/module_vpc.tf
@@ -1,10 +1,1 @@
-module "module_vpc" {
-source                 = "./vpc/"
-vpc_name               = "georgevpc"
-vpc_id                 = "aws_vpc.george_vpc.id"
-vpc_cidr_block         = "10.0.0.0/16"
-vpc_availability_zones = ["us-east-1a", "us-east-1b"]
-vpc_public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
-vpc_private_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
-vpc_tags               = geogre_ibrahim
-}
+
