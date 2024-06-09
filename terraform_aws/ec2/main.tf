3e371dd639f6a5e24a2a1395c296c40bcc8bb8c3
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 22:06:09 -0400

    Update main.tf

--- terraform_aws/ec2/main.tf
+++ terraform_aws/ec2/main.tf
@@ -11,7 +11,7 @@ data "aws_ami" "amzn-linux-2023-ami" {
 resource "aws_instance" "george" {
   ami           = var.ami
   instance_type = var.instance_type
-  subnet_id     = module.vpc.vpc_id
+  subnet_id     = var.subnet_id
 
   cpu_options {
     core_count       = 2
