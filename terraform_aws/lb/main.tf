654ce6cc1af0b878e6b8edb4db0e0e2cd1857d66
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-09 09:32:36 -0400

    Update main.tf

--- terraform_aws/lb/main.tf
+++ terraform_aws/lb/main.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "george" {
   name               = "george-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [moudle.sg]
+  security_groups    = [module.sg]
   subnets            = [module.vpc]
 
   enable_deletion_protection = true
