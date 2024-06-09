2bd12d46433b6a0f0cd6b149d64590af194ae85b
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 23:10:40 -0400

    Update module_ec2.tf

--- terraform_aws/module_ec2.tf
+++ terraform_aws/module_ec2.tf
@@ -4,6 +4,5 @@ module "ec2" {
   ami                  = "data.aws_ami.amzn-linux-2023-ami.id"
   instance_type        = "c6a.2xlarge"
   tags                 = "tf-george"
-  subnet_id            = "aws_subnet.george.id"
 }
 
