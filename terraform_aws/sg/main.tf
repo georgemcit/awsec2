3aad2715f2d9fd335fe87664dbc5d88267a8dfff
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 22:57:47 -0400

    Update main.tf

--- terraform_aws/sg/main.tf
+++ terraform_aws/sg/main.tf
@@ -1,6 +1,4 @@
 resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc" "george"
 
   tags = {
