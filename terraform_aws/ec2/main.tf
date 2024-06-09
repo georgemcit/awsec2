a207de2732f2b986a8b513a104df891954ef73a5
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 20:48:26 -0400

    Create main.tf

---
+++ terraform_aws/ec2/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+resource "aws_instance" "george" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
+  cpu_options {
+    core_count       = 2
+    threads_per_core = 2
+  }
+
+  tags = {
+    Name = var.tags
+  }
+}
