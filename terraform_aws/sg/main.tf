62d3a9275159edf584dc69eb04d265defa4b1e50
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 22:59:06 -0400

    Update main.tf

--- terraform_aws/sg/main.tf
+++ terraform_aws/sg/main.tf
@@ -1,23 +1,21 @@
-resource "aws_security_group" "allow_tls" {
-  vpc_id      = aws_vpc" "george"
-
-  tags = {
-    Name = "allow_tls"
-  }
+resource "aws_security_group" "vpc-ssh" {
+    name="vpc-ssh"
+    description = "Dev VPC SSH"
+    ingress{
+        description="Allow port 22"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    egress{
+        description = "Allow all ip and ports outbound"
+        from_port=0
+        to_port=0
+        protocol="-1"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    tags = {
+      Name="vpc-ssh"
+    }
 }
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
-
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
