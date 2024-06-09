b7f95f7c63bf1d11da220647cdb35ca714f93120
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 20:59:44 -0400

    Update module_ec2.tf

--- terraform_aws/module_ec2.tf
+++ terraform_aws/module_ec2.tf
@@ -2,10 +2,4 @@ module "module_ec2" {
   source            = "./ec2/"
   ami               = "data.aws_ami.amzn-linux-2023-ami.id"
   instance_type     = "c6a.2xlarge"
-  subnet_id         = "aws_subnet.george.id"
-  vpc_cidr_block    = "172.16.0.0/16"
-  vpc_id            = "aws_vpc.george.id"
-  sb_cidr_block     = "172.16.10.0/24"
-  tags              = "tf-george"
-  availability_zone = "us-east-2a"
 }
